bafdf66912a3eca426c9b4363518d95f46336ca3
NaveenKumar <120005658+nkr1320@users.noreply.github.com>
2025-05-31 17:01:38 +0530

    Update LightingSection.jsx
    for smaller devices images size adjusted

--- src/TailwindBlocks/Components/Pages/LightingSection.jsx
+++ src/TailwindBlocks/Components/Pages/LightingSection.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 import light1 from '../Pages/images/light1.jpg';
 import light2 from '../Pages/images/light2.jpg';
 import light3 from '../Pages/images/light3.png';
@@ -9,48 +8,49 @@ import light5 from '../Pages/images/light5.jpg';
 import light6 from '../Pages/images/light6.png';
 
 const LightingSection = () => {
+  return (
+    <>
+      <section id="ContentSection" className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto flex flex-wrap">
 
+          <div className="flex flex-col items-center text-center w-full mb-20 lg:flex-row lg:items-start lg:justify-start lg:text-center">
+            <h1 className="text-2xl sm:text-3xl font-medium title-font text-gray-900 mb-4 lg:mb-0 lg:w-1/3 lg:mr-4 ">
+              Ceiling Light
+            </h1>
+            <p className="leading-relaxed text-base px-4 lg:px-0 lg:pl-6 lg:w-2/3">
+              Elevate your ambiance with expertly curated ceiling lighting. From subtle glow to statement fixtures, we tailor every detail. Lighting that not only brightens but defines your space.
+            </p>
+          </div>
 
-    return (<>
-   <section id="ContentSection" className="text-gray-600 body-font">
-  <div className="container px-5 py-24 mx-auto flex flex-wrap">
+          <div className="flex flex-wrap md:-m-2 -m-1">
+            <div className="flex flex-wrap w-full md:w-1/2">
+              <div className="md:p-2 p-1 w-full md:w-1/2">
+                <img alt="gallery" className="w-full object-cover h-full object-center block rounded-2xl" src={light1} />
+              </div>
+              <div className="md:p-2 p-1 w-full md:w-1/2">
+                <img alt="gallery" className="w-full object-cover h-full object-center block rounded-2xl" src={light2} />
+              </div>
+              <div className="md:p-2 p-1 w-full">
+                <img alt="gallery" className="w-full h-full object-cover object-center block rounded-2xl" src={light3} />
+              </div>
+            </div>
+            <div className="flex flex-wrap w-full md:w-1/2">
+              <div className="md:p-2 p-1 w-full">
+                <img alt="gallery" className="w-full h-full object-cover object-center block rounded-2xl" src={light4} />
+              </div>
+              <div className="md:p-2 p-1 w-full md:w-1/2">
+                <img alt="gallery" className="w-full object-cover h-full object-center block rounded-2xl" src={light5} />
+              </div>
+              <div className="md:p-2 p-1 w-full md:w-1/2">
+                <img alt="gallery" className="w-full object-cover h-full object-center block rounded-2xl" src={light6} />
+              </div>
+            </div>
+          </div>
 
-   <div className="flex flex-col items-center text-center w-full mb-20 lg:flex-row lg:items-start lg:justify-start lg:text-center">
-  <h1 className="text-2xl sm:text-3xl font-medium title-font text-gray-900 mb-4 lg:mb-0 lg:w-1/3 lg:mr-4 ">
-    Ceiling Light
-  </h1>
-  <p className="leading-relaxed text-base px-4 lg:px-0 lg:pl-6 lg:w-2/3">
-    Elevate your ambiance with expertly curated ceiling lighting. From subtle glow to statement fixtures, we tailor every detail. Lighting that not only brightens but defines your space.
-  </p>
-</div>
-
-    <div className="flex flex-wrap md:-m-2 -m-1">
-      <div className="flex flex-wrap w-1/2">
-        <div className="md:p-2 p-1 w-1/2">
-          <img alt="gallery" className="w-full object-cover h-full object-center block rounded-2xl" src={light1} />
         </div>
-        <div className="md:p-2 p-1 w-1/2">
-          <img alt="gallery" className="w-full object-cover h-full object-center block rounded-2xl" src={light2} />
-        </div>
-        <div className="md:p-2 p-1 w-full">
-          <img alt="gallery" className="w-full h-full object-cover object-center block rounded-2xl" src={light3} />
-        </div>
-      </div>
-      <div className="flex flex-wrap w-1/2">
-        <div className="md:p-2 p-1 w-full">
-          <img alt="gallery" className="w-full h-full object-cover object-center block rounded-2xl" src={light4} />
-        </div>
-        <div className="md:p-2 p-1 w-1/2">
-          <img alt="gallery" className="w-full object-cover h-full object-center block rounded-2xl" src={light5} />
-        </div>
-        <div className="md:p-2 p-1 w-1/2">
-          <img alt="gallery" className="w-full object-cover h-full object-center block rounded-2xl" src={light6} />
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-    </>);
-}
+      </section>
+    </>
+  );
+};
 
 export default LightingSection;
